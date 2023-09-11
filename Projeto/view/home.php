b75332921f0ae17565ea38e437c20d06cd57bb85
Larissa Rodrigues Da Silva <lariedream@rede.ulbra.br>
2023-09-11 20:28:22 -0300

    Primeiro commit

---
+++ Projeto/view/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina inicial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </head>
+<body style="background-color:#556594;">
+  
+  <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/' . FOLDER . '/view/navbar.php'; ?>
+
+  <div class="container">
+    <div class="row text-center">
+      <h2 style="font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;">Atividade AP1 - Larissa Rodrigues da Silva</h2>
+
+      <div class="text-center ">
+        <img class="img-fluid" src="https://static.imasters.com.br/wp-content/uploads/2018/06/22153245/php-post-1.png" alt="Imagem representativa da linguagem de PHP ">
+      </div>
+    </div>
+  </div>
+</body>
+</html>
